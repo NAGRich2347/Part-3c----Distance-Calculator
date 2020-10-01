a4456d2c8b7210c34be4d34cdb0a100d0407345f
Noah Richardson <RichNoah983@GoTVDSB.ca>
2020-10-01 12:20:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_3c____Distance_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distanceOne;
+            Console.WriteLine("Please enter your first distance (kmph)");
+            distanceOne = Convert.ToInt32(Console.ReadLine());
+
+            int distanceTwo;
+            Console.WriteLine("Please enter your first distance (kmph)");
+            distanceTwo = Convert.ToInt32(Console.ReadLine());
+            
+            int distanceThree;
+            Console.WriteLine("Please enter your first distance (kmph)");
+            distanceThree = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Press any key to find the average of your distances.");
+            Console.ReadLine();
+
+            int distanceSum = Convert.ToInt32($"{distanceOne} + {distanceTwo} + {distanceThree}");
+            distanceSum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The sum of your distances is {distanceSum}");
+            Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}
